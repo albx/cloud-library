c3876bf7e60d5f896118cf814b8791177dce0b8c
Alberto Mori <albi.mo87@hotmail.it>
2024-03-04 07:01:29 +0100

    configure strawberryshake client

--- CloudLibrary.Client/Program.cs
+++ CloudLibrary.Client/Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddLibraryClient()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri($"{builder.HostEnvironment.BaseAddress}data-api/graphql"));
 
 await builder.Build().RunAsync();
