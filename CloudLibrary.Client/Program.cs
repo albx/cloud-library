edfc027da56cd7a7af493e148205f85b1312c814
Alberto Mori <albi.mo87@hotmail.it>
2024-03-11 06:55:17 +0100

    setup authentication in swa

--- CloudLibrary.Client/Program.cs
+++ CloudLibrary.Client/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CloudLibrary.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using CloudLibrary.Client.Providers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +11,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services
     .AddLibraryClient()
     .ConfigureHttpClient(client => client.BaseAddress = new Uri($"{builder.HostEnvironment.BaseAddress}data-api/graphql"));
+
+builder.Services.AddHttpClient(
+    "AuthClient",
+    client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+builder.Services
+    .AddAuthorizationCore()
+    .AddScoped<AuthenticationStateProvider, CloudLibraryAuthenticationStateProvider>();
 
 await builder.Build().RunAsync();
---
+++ CloudLibrary.Client/Providers/CloudLibraryAuthenticationStateProvider.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Json;
+using System.Security.Claims;
+
+namespace CloudLibrary.Client.Providers;
+
+public class CloudLibraryAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public CloudLibraryAuthenticationStateProvider(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        try
+        {
+            using var httpClient = _httpClientFactory.CreateClient("AuthClient");
+
+            var userInfo = await httpClient.GetFromJsonAsync<UserInformation>(".auth/me");
+            var clientPrincipal = userInfo!.ClientPrincipal;
+
+            if (clientPrincipal is not null)
+            {
+                var identity = new ClaimsIdentity(clientPrincipal.IdentityProvider);
+                if (!string.IsNullOrEmpty(clientPrincipal.UserId))
+                {
+                    identity.AddClaim(new(ClaimTypes.NameIdentifier, clientPrincipal.UserId));
+                }
+                if (!string.IsNullOrEmpty(clientPrincipal.UserDetails))
+                {
+                    identity.AddClaim(new(ClaimTypes.Name, clientPrincipal.UserDetails));
+                }
+                if (clientPrincipal.UserRoles.Any())
+                {
+                    identity.AddClaims(clientPrincipal.UserRoles
+                        .Where(r => !r.Equals("anonymous", StringComparison.InvariantCultureIgnoreCase))
+                        .Select(r => new Claim(ClaimTypes.Role, r)));
+                }
+            }
+
+            return new AuthenticationState(new ClaimsPrincipal());
+        }
+        catch 
+        {
+            return new AuthenticationState(new ClaimsPrincipal());
+        }
+    }
+
+    internal class UserInformation
+    {
+        public ClientPrincipal? ClientPrincipal { get; set; }
+    }
+
+    internal record ClientPrincipal
+    {
+        public string IdentityProvider { get; init; } = string.Empty;
+
+        public string UserId { get; init; } = string.Empty;
+
+        public string UserDetails { get; init; } = string.Empty;
+
+        public string[] UserRoles { get; init; } = [];
+    }
+}
